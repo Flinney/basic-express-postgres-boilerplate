452bb467aeb8df8283be193eeac03147b5bc0e15
Austin Binkley <77643261+Flinney@users.noreply.github.com>
2021-10-16 09:16:41 -0500

    Upload most files

---
+++ knexfile.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const {	DATABASE_URL } = process.env;
+
+module.exports = {
+	development: {
+		client: "postgresql",
+		connection: DATABASE_URL,
+		pool: { min: 0, max: 5 },
+		migrations: {
+			directory: path.join(__dirname, "src", "db", "migrations"),
+		},
+		seeds: {
+			directory: path.join(__dirname, "src", "db", "seeds"),
+		},
+	},
+
+	production: {
+		client: "postgresql",
+		connection: DATABASE_URL,
+		pool: { min: 0, max: 5 },
+		migrations: {
+			directory: path.join(__dirname, "src", "db", "migrations"),
+		},
+		seeds: {
+			directory: path.join(__dirname, "src", "db", "seeds"),
+		},
+	},
+
+	test: {
+		client: "postgresql",
+		connection: DATABASE_URL,
+		migrations: {
+			directory: path.join(__dirname, "src", "db", "migrations"),
+		},
+		seeds: {
+			directory: path.join(__dirname, "src", "db", "seeds"),
+		},
+		useNullAsDefault: true,
+	},
+};
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.status(200).send(`Hello World!`);
+});
+
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
